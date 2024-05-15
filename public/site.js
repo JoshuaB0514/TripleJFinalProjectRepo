5eebe3ba74dcac15d53ab834928bb9392c7b6ef1
Joshua Bohn <500200388@fvtc.edu>
2024-05-15 13:04:55 -0500

    Made functional code for all 3 of the get endpoints, HOPEFULLY did the same with the post endpoints, and modified other endpoints a bit so they connected to our mongodb

--- public/site.js
+++ public/site.js
@@ -27,7 +27,7 @@ app.use((request, response) => {
 
 const getCollection = async (dbName, collectionName) => {
     await client.connect()
-    return client.db('todoAPI').collection(collectionName)
+    return client.db('dbName').collection(collectionName)
 }
 
 app.locals.getCollection = getCollection
@@ -35,26 +35,61 @@ app.locals.getCollection = getCollection
 const message = `Server running: http://localhost:${port}`
 app.listen(port, () => console.log(message))
 
-router.put('/:id', async (req, res) => {
-    const collection = await getCollection('dbName1', 'collectionName')
+router.get('/api/menu', async (req, res) => {
+    const collection = await getCollection('Food-Truck-API', 'Menu')
+	const menuitems = await collection.find().toArray()
+	const menuId = menuitems.map((menuitem) => { return { ...menuitem, id: menuitem._id }; })
+	res.json(menuId)
+})
+
+router.get('/api/events', async (req, res) => {
+    const collection = await getCollection('Food-Truck-API', 'Events')
+	const events = await collection.find().toArray()
+	const eventId = events.map((event) => { return { ...event, id: event._id }; })
+	res.json(eventId)
+})
+
+router.get('/api/events/:id', async (req, res) => {
+    const { id } = req.params
+    const collection = await getCollection('Food-Truck-API', 'Events')
+	const event = await collection.findOne({ "_id": new ObjectId(id) })
+	res.json(event)
+})
+
+app.post('/api/menu', async (req, res) => {
+    const item = req.body
+    const collection = await getCollection('Food-Truck-API', 'Menu')
+    const result = await collection.insertOne(item)
+    res.json(result)
+});
+
+app.post('/api/events', async (req, res) => {
+    const item = req.body
+    const collection = await getCollection('Food-Truck-API', 'Events')
+    const result = await collection.insertOne(item)
+    res.json(result)
+});
+
+router.put('/api/menu/:id', async (req, res) => {
+    const collection = await getCollection('Food-Truck-API', 'Menu')
     const result = await collection.updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body })
     res.json(result)
 })
 
-router.delete('/:id', async (req, res) => {
-    const collection = await getCollection('dbName1', 'collectionName')
+router.delete('/api/menu/:id', async (req, res) => {
+    const collection = await getCollection('Food-Truck-API', 'Menu')
     const result = await collection.deleteOne({ _id: ObjectId(req.params.id) })
     res.json(result)
 })
 
-router.put('/:id', async (req, res) => {
-    const collection = await getCollection('dbName2', 'collectionName')
+router.put('/api/events/:id', async (req, res) => {
+    const collection = await getCollection('Food-Truck-API', 'Events')
     const result = await collection.updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body })
     res.json(result)
 })
 
-router.delete('/:id', async (req, res) => {
-    const collection = await getCollection('dbName2', 'collectionName')
+router.delete('/api/events/:id', async (req, res) => {
+    const collection = await getCollection('Food-Truck-API', 'Events')
     const result = await collection.deleteOne({ _id: ObjectId(req.params.id) })
     res.json(result)
 })
